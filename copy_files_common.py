c3285dbee0c501c18289ffb5dc67853da3da1954
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-07-07 11:01:27 -0500

    Update windows script to cuda 8.0.61 with cupti

--- copy_files_common.py
+++ copy_files_common.py
@@ -18,6 +18,10 @@ cuda_libraries = [
     'nvrtc',
 ]
 
+extras_libraries = [
+    'cupti',
+]
+
 libdevice_versions = ['20.10', '30.10', '35.10', '50.10']
 
 
@@ -32,12 +36,13 @@ def get_paths(libraries, dirpath, template):
 
 
 def copy_files(cuda_lib_dir, cuda_lib_fmt, nvvm_lib_dir, nvvm_lib_fmt,
-               libdevice_lib_dir, libdevice_lib_fmt):
+               libdevice_lib_dir, libdevice_lib_fmt, extras_lib_dir, extras_lib_fmt):
     filepaths = []
     filepaths += get_paths(cuda_libraries, cuda_lib_dir, cuda_lib_fmt)
     filepaths += get_paths(['nvvm'], nvvm_lib_dir, nvvm_lib_fmt)
     filepaths += get_paths(libdevice_versions, libdevice_lib_dir,
                            libdevice_lib_fmt)
+    filepaths += get_paths(extras_libraries, extras_lib_dir, extras_lib_fmt)
 
     for fn in filepaths:
         print('copying', fn)
--- copy_files_windows.py
+++ copy_files_windows.py
@@ -4,12 +4,14 @@ from copy_files_common import copy_files
 
 
 def main():
-    copy_files(cuda_lib_dir=r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\bin',
-               cuda_lib_fmt='{0}64_75.dll',
-               nvvm_lib_dir=r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\nvvm\bin',
-               nvvm_lib_fmt='{0}64_30_0.dll',
-               libdevice_lib_dir=r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v7.5\nvvm\libdevice',
-               libdevice_lib_fmt='libdevice.compute_{0}.bc')
+    copy_files(cuda_lib_dir=r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin',
+               cuda_lib_fmt='{0}64_80.dll',
+               nvvm_lib_dir=r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\nvvm\bin',
+               nvvm_lib_fmt='{0}64_31_0.dll',
+               libdevice_lib_dir=r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\nvvm\libdevice',
+               libdevice_lib_fmt='libdevice.compute_{0}.bc',
+               extras_lib_dir=r'C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\extras\CUPTI\libx64',
+               extras_lib_fmt='{0}64_80.dll')
 
 
 if __name__ == '__main__':
